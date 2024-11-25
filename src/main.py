33f7de441d8083f4e2c3574dce7c546ac9b9a9b4
R Ajai Iyer -X (rajaiyer - LTIMINDTREE LIMITED at Cisco) <rajaiyer@cisco.com>
2024-11-25 18:17:57 +0530

    Added multi-threading in main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+import threading
 from function1 import func1
 from function2 import func2
 
-def main():
-    print(func1())
-    print(func2())
+def run_in_threads():
+    t1 = threading.Thread(target=lambda: print(func1()))
+    t2 = threading.Thread(target=lambda: print(func2()))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
 
 if __name__ == "__main__":
-    main()
+    run_in_threads()
